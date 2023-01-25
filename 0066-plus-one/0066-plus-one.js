c9f80a9916eeab9adfee9283e3730847c5df2523
Assegid Assefa <77196618+assegidasefa@users.noreply.github.com>
2023-01-25 11:05:12 -0800

    Time: 69 ms (56.86%), Space: 41.6 MB (92.87%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -8,10 +8,8 @@ var plusOne = function(digits) {
     let arr = []
     for (let i = 0 ; i<digits.length;i++){
         str+=digits[i]
-        console.log("str",str)
     }
     num =BigInt(str) + BigInt(1)
-    console.log("strnum",num)
     while(num>0){
         let val = num % 10n   
         num = num/10n
